11aad0f4d0e32e2732d4540cf7dfe46ee38328b1
Himadri Ghosh <144557551+HIMA6768@users.noreply.github.com>
2026-09-04 10:40:18 +0530

    Update silver_load_procedure.sql

--- scripts/silver/silver_load_procedure.sql
+++ scripts/silver/silver_load_procedure.sql
@@ -1,1 +1,44 @@
 
+-- inserting transformed data in customer_info
+truncate table  silver.crm_cust_info;
+insert into   silver.crm_cust_info 
+(cust_id, 
+    cust_key, 
+    cust_firstname, 
+    cust_lastname, 
+    cust_marital_status, 
+    cust_gender, 
+    cust_create_date)
+	
+with rev_cust_info as 
+(
+select*,
+ROW_NUMBER()
+over (
+ partition by cust_id 
+ order by cust_create_date desc
+) as rank
+from bronze.crm_cust_info
+)
+select 
+cust_id,
+trim(cust_key) as cust_key,
+trim(cust_firstname)as cust_firstname,
+trim(cust_lastname) as cust_lastname,
+
+case
+when upper(cust_marital_status)='M' then 'Married'
+when upper(cust_marital_status)='S' then 'Single'
+else 'NA'
+end as cust_marital_status,
+
+case
+when upper(cust_gender)='M' then 'Male'
+when upper(cust_gender)='F' then 'Female'
+else 'NA'
+end as cust_gender,
+
+cust_create_date
+
+from rev_cust_info
+where rank=1 and cust_id is not NULL;
