1fe23897007b5fed78c6e890512da8e7a4eccd7e
Himadri Ghosh <144557551+HIMA6768@users.noreply.github.com>
2026-09-05 10:04:10 +0530

    Create view_dimension_product.sql

---
+++ scripts/gold/view_dimension_product.sql
@@ -0,0 +1,15 @@
+create view gold.dim_product as
+select 
+row_number() over(order by pi.prd_id) as product_key,
+pi.prd_id as product_id,
+pi.prd_name as product_name,
+pi.prd_price as product_price,
+pi.prd_line as product_line,
+pc.pid as catagory_id,
+pc.cat as category,
+pc.subcat as subcategory,
+pi.prd_start_date as startdate,
+pc.maintainance as maintainance
+from silver.crm_prd_info pi
+left join silver.erp_px_cat  pc
+on pi.cat_key=pc.pid
